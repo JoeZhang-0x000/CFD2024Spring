1d97fa636ae60cc42e27d3bfd4a697e4007d3360
zhangx <1772874955@qq.com>
2024-04-07 14:53:11 +0800

    '第五讲作业'

--- chapter5/FD.m
+++ chapter5/FD.m
@@ -1,43 +1,120 @@
-% % 差分方法的基类，创建时候指定不同的格式，调用时候使用forward()
-% classdef FD < handle
-%     properties
-%         id = 0;
-%     end
-% 
-%     methods 
-%         % 根据id创建差分格式
-%         function obj = FD(id)
-%             obj.id = id;
-%         end
-% 
-%         function obj = setId(obj,id)
-%             obj.id = id;
-%         end
-%         
-%         % 根据method_id计算差分
-%         function u_x = forward(obj,u,dx)
-%             n = length(u);
-%             switch obj.id
-%                 case -1 % 前差, (f_{j+1} - f_{j}) / dx
-%                     u_t = [u(2:n)];
-%                     u_diff = u_t - u(1:n-1);
-%                     u_x = u_diff / dx;
-%                 case 0 % 
-%                     error('not implement error!');
-%                 case 1 % 后差, (f_{j} - f_{j-1}) / dx
-%                     u_t = [u(1:n-1)]; % 左移一位
-%                     u_diff = u(2:n) - u_t;
-%                     u_x = u_diff / dx;
-%                 case 3 % GVC2
-%                     % only cover 2 to n-1
-%                     u_1 = [u(1:n-2)]; % u_{j-1} from 1 to n-2
-%                     u_2 = [u(3:n)]; % u_{j+1}, from 3 to n
-%                     u_ = [u(2:n-1)]; % u_{j}, from 2 to n-1
-%                     limiter = (u_ - u_1) / (u_2 - u_);
-%                     u_r = u_ + limiter .* (u_2 - u_) / 2; % u_{j+1/2}, from 2 to n-1
-%                     u_l = u_1 + limiter .* 
-%             end
-%         end
-% 
-%     end
-% end
+% % 差分方法的基类，创建时候指定不同的格式
+classdef FD < handle
+    properties
+        id = 0;
+    end
+
+    methods
+        % 根据id创建差分格式
+        function obj = FD(id)
+            obj.id = id;
+        end
+
+        function obj = setId(obj,id)
+            obj.id = id;
+        end
+
+        % 根据method_id计算差分
+        function u_x = computeDerivative(obj,u,dx)
+            switch obj.id
+                case 1 % 前差, (f_{j+1} - f_{j}) / dx
+                    u_x = obj.forward(u,dx);
+                case 2 % 后差, (f_{j} - f_{j-1}) / dx
+                    u_x = obj.backward(u,dx);
+                case 3 % TVD
+                    u_x = obj.TVD(u,dx);
+                case 4 % NNd
+                    u_x = obj.NND(u,dx);
+            end
+        end
+
+        function u_x = forward(obj,u,dx) % 前差, (f_{j+1} - f_{j}) / dx
+            u_tem = [u(2:length(u)),u(length(u))]; % padding
+            u_diff = u_tem - u;
+            u_x = u_diff / dx;
+        end
+
+        function u_x = backward(obj,u,dx) % 后差, (f_{j} - f_{j-1}) / dx
+            u_tem = [u(1),u(1:length(u)-1)]; % padding
+            u_diff = u - u_tem;
+            u_x = u_diff / dx;
+        end
+
+        function Fx = TVD(obj,u,dx) % TVD
+            [N,M] = size(u);
+            if N ~= 2
+                error('未定义');
+            end
+            F_p = u(1,:);
+            F_n = u(2,:);
+            Fh_p = zeros(1,M);
+            Fh_n = zeros(1,M);
+            Fx_p = zeros(1,M);
+            Fx_n = zeros(1,M);
+            Fx = zeros(1,M);
+            xs = 2;
+            xt = M - xs;
+
+            for j = xs : xt
+                r_p = (F_p(j) - F_p(j - 1)) ./ (F_p(j + 1) - F_p(j) + eps); 
+                r_n = (F_n(j + 2) - F_n(j + 1)) ./ (F_n(j + 1) - F_n(j) + eps);
+                Phi_p = (r_p + abs(r_p)) ./ (1 + r_p);
+                Phi_n = (r_n + abs(r_n)) ./ (1 + r_n);
+
+                Fh_p(j) = F_p(j) + 0.5 * (Phi_p .* (F_p(j + 1) - F_p(j)));
+                Fh_n(j) = F_n(j + 1) - 0.5 * (Phi_n .* (F_n(j + 1) - F_n(j)));
+            end
+            
+            xs_new = xs + 1;
+            xt_new = xt;
+
+            for j = xs_new : xt_new
+                Fx_p(j) = (Fh_p(j) - Fh_p(j - 1)) / dx;
+                Fx_n(j) = (Fh_n(j) - Fh_n(j - 1)) / dx;
+                Fx(j) = Fx_p(j) + Fx_n(j);
+            end
+        end
+
+        function Q = Minmod(obj,a, b)
+            Q = 0;
+            if a * b > 0
+                Q = a;
+                if abs(a) > abs(b)
+                    Q = b;
+                end
+            end
+
+        end
+
+        function Fx = NND(obj,u,dx) % NND
+            [N,M] = size(u);
+            if N ~= 2
+                error('未定义');
+            end
+            F_p = u(1,:);
+            F_n = u(2,:);
+            Fh_p = zeros(1,M);
+            Fh_n = zeros(1,M);
+            Fx_p = zeros(1,M);
+            Fx_n = zeros(1,M);
+            Fh = zeros(1,M);
+            Fx = zeros(1,M);
+            xs = 2;
+            xt = M - xs;
+
+            for j = xs : xt
+                Fh_p(j) = F_p(j) +     (0.5 * obj.Minmod((F_p(j) - F_p(j - 1)), (F_p(j + 1) - F_p(j))));
+                Fh_n(j) = F_n(j + 1) - (0.5 * obj.Minmod((F_n(j + 1) - F_n(j)), (F_n(j + 2) - F_n(j + 1))));
+                Fh(j) = Fh_p(j) + Fh_n(j);
+            end
+
+            xs_new = xs + 1;
+            xt_new = xt;
+
+            for j = xs_new : xt_new
+                Fx(j) = (Fh(j) - Fh(j - 1)) / dx;
+            end
+        end
+
+    end
+end
--- chapter5/MySod.m
+++ chapter5/MySod.m
@@ -14,8 +14,6 @@ classdef MySod < handle
         gamma;
         dt;
         t;
-        ep = 1e-6;
-        f_x;
     end
 
     methods
@@ -62,52 +60,29 @@ classdef MySod < handle
             obj.num = length(obj.x);
         end
 
-        function fvs(obj)
-            % 计算lambda_1, lambda_2, lambda_3
-            lam = zeros(3,obj.num);
-            lam(1,:) = obj.u;
-            lam(2,:) = obj.u - obj.c;
-            lam(3,:) = obj.u + obj.c;
-            lam_p = zeros(3,obj.num); % postive lambda_k
-            lam_n = zeros(3,obj.num); % negative lambda_k
 
-            % 计算 lambda+, lambda-
-            for i = 1:3
-                lam_p(i,:) = (lam(i,:) + sqrt(lam(i,:).^2+obj.ep)) / 2;
-                lam_n(i,:) = (lam(i,:) - sqrt(lam(i,:).^2+obj.ep)) / 2;
+        function Q = stepFun(obj,FD,u)
+            % 显式推进的方法
+            % u_t = -f_x = Q 
+            if ~ isa(FD,'FD')
+                error('差分方法错误');
             end
 
-            % 计算f+, f-
-            w = (3-obj.gamma) * (lam_p(2,:)+lam_p(3,:)) .* (obj.c.^2) / (obj.gamma - 1) / 2;
-            f_p = obj.rho / (2*obj.gamma) .* [
-                2* (obj.gamma-1) * lam_p(1,:) + lam_p(2,:) + lam_p(3,:);
-                2* (obj.gamma-1) * lam_p(1,:) .* obj.u + lam_p(2,:) .* (obj.u - obj.c) + lam_p(3,:) .* (obj.u + obj.c);
-                (obj.gamma - 1) * lam_p(1,:) .* (obj.u).^2 + lam_p(2,:) / 2 .* (obj.u - obj.c).^2 + lam_p(3,:) / 2 .* (obj.u + obj.c).^2 + w;
-                ];
-
-            w = (3-obj.gamma) * (lam_n(2,:)+lam_n(3,:)) .* (obj.c.^2) / (obj.gamma - 1) / 2;
-            f_n = obj.rho / (2*obj.gamma) .* [
-                2* (obj.gamma-1) * lam_n(1,:) + lam_n(2,:) + lam_n(3,:);
-                2* (obj.gamma-1) * lam_n(1,:) .* obj.u + lam_n(2,:) .* (obj.u - obj.c) + lam_n(3,:) .* (obj.u + obj.c);
-                (obj.gamma - 1) * lam_n(1,:) .* (obj.u).^2 + lam_n(2,:) / 2 .* (obj.u - obj.c).^2 + lam_n(3,:) / 2 .* (obj.u + obj.c).^2 + w;
-                ];
-            %             disp(f_p);
-            %             disp(f_n);
-            % 利用不同迎风格式计算 \partial f^+ / \partial x, \partial f^- / \partial x
-            % 默认调用nnd格式
-            f_n_x = GVC2(f_n, obj.dx);
-            f_p_x = GVC2(f_p, obj.dx);
-
-            % 令 f_x = f_n_x + f_p_x
-            obj.f_x = f_n_x + f_p_x;
+            fvs = FVS;
+            [f_p, f_n] = fvs.simple(u,obj.gamma);
+            for i = 1:3
+                tem = [f_p(i,:);f_n(i,:)];
+                f_x(i,:) = FD.computeDerivative(tem,obj.dx);
+            end
+            
+            Q = -f_x;
         end
 
-        function timeStep(obj, fvs, rk)
+        function obj = timeStep(obj,fd, rk)
             % 时间推进，默认三阶R-K
-            fd = @(u,dx) GVC2(u,dx);
-            fun = @(u,dx) fvs.sw(u,dx,obj.gamma,obj.ep);
+            func = @(u) obj.stepFun(fd,u);
             U = [obj.rho; obj.rho .* obj.u; obj.E]; % [3,num]
-            U = rk.rk3(U,fun,obj.dt,obj.dx); % 下一个时刻的 rho, rho * u, E
+            U = rk.rk3(U,func,obj.dt); % 下一个时刻的 rho, rho * u, E
 
             % 更新
             obj.rho = U(1,:); % rho = rho
